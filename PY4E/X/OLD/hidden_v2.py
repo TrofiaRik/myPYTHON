82e26b06520724e4e8ba2de537f3d8abaca14e6e
Riccardo Trofia Pavia <riccardo.pavia.2024@gmail.com>
2025-03-21 09:11:59 +0100

    hidden_v2.py

--- PY4E/X/OLD/hidden_v2.py
+++ PY4E/X/OLD/hidden_v2.py
@@ -1,2 +1,2 @@
 def oauth():
-    return {"bearer_token": "AAAAAAAAAAAAAAAAAAAAALxtygEAAAAArmqt8tPFDxcV8vdSKflg5vlK8oY%3DtYOcJL1PsmRZSg59LDrsKhBwlmVDGsYEHGOKxewm2pVTZakf6T"}
+    return {"bearer_token": "mybearertoken"}
